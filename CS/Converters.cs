85fd56c3eb462dc1928337687979b809c548547a
Dmitry Romenskiy (DevExpress) <romenskiy.dmitry@corp.devexpress.com>
2020-12-31 13:39:33 +0300

    cleanup

---
+++ CS/Converters.cs
@@ -0,0 +1,40 @@
+﻿using DevExpress.Data.Filtering;
+using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace FilterCombo {
+    public class FilterConverter : MarkupExtension, IValueConverter {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            return CriteriaOperator.Parse("[FilterType] == ?", value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+        public override object ProvideValue(IServiceProvider serviceProvider) {
+            return this;
+        }
+    }
+
+    public class MultiBindingConverter : MarkupExtension, IMultiValueConverter {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) {
+            if((bool)values[2]) {
+                var fType = (RowFilterType)values[1];
+                return ((ObservableCollection<ComboItem>)values[0]).Where(f => f.FilterType.Equals(fType));
+            } else return values[0];
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider) {
+            return this;
+        }
+    }
+}
---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+
+namespace FilterCombo {
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
